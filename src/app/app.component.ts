f14a6540124857474f3ca7dfdb719329958b5fa5
Murat Gündüz <mrt.gunduz25@gmail.com>
2024-05-22 00:34:29 +0300

    END !

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'dynamicformsgroup';
+export class AppComponent implements OnInit {
+  orderForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.orderForm = this.formBuilder.group({
+      items: this.formBuilder.array([]),
+    });
+    this.addItem();
+  }
+
+  get items(): FormArray {
+    return this.orderForm.get('items') as FormArray;
+  }
+
+  createItem(): FormGroup {
+    return this.formBuilder.group({
+      name: ['', Validators.required],
+      description: [''],
+      price: ['', Validators.required],
+    });
+  }
+
+  addItem() {
+    this.items.push(this.createItem());
+  }
+
+  deleteItem(index: number) {
+    this.items.removeAt(index);
+  }
 }
